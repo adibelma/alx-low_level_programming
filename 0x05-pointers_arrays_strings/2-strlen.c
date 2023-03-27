71d62cc163646f74f1348b2c499419bdf8c838d9
adibelma <elmarbout.adib@gmail.com>
2023-03-27 13:20:11 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,46 +1,18 @@
 #include "main.h"
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	int i, d, n, len, f, digit;
+	int longi = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	while (*s != '\0')
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		longi++;
+		s++;
 	}
 
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (longi);
 }
