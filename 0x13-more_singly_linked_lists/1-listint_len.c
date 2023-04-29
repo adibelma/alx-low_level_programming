0c2c607cbb79c021e14064d677ba5b929db46fb4
adibelma <elmarbout.adib@gmail.com>
2023-04-29 10:59:37 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t numb = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		numb++;
+		num++;
 		h = h->next;
 	}
 
-	return (numb);
+	return (num);
 }
