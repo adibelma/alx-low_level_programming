28fe2d855391640b98f580ed1b991ac3b9f924d3
adibelma <elmarbout.adib@gmail.com>
2023-04-29 10:32:30 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number_n = 0;
+	size_t number = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		number_n++;
+		number++;
 		h = h->next;
 	}
 
-	return (number_n);
+	return (number);
 }
