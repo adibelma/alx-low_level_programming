831ad73448d19b0e603ecbffe4f55784fc614960
adibelma <elmarbout.adib@gmail.com>
2023-05-02 15:17:10 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - returns the number of elements in a linked listint_t  lists
+ * @h: linked list of type listint_t to reverse
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t number = 0;
 
 	while (h)
 	{
-		num++;
+		number++;
 		h = h->next;
 	}
 
-	return (num);
+	return (number);
 }
 
