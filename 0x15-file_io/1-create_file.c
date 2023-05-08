20c3316882926209ff1a256b2d795e575c8bb636
adibelma <elmarbout.adib@gmail.com>
2023-05-08 12:06:19 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int wr, f, ln = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (ln = 0; text_content[ln];)
+			ln++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(f, text_content, ln);
 
-	if (fd == -1 || w == -1)
+	if (f == -1 || wr == -1)
 		return (-1);
 
-	close(fd);
+	close(f);
 
 	return (1);
 }
