6591014d1bf4ef3e591f306612d6f024effe29f3
adibelma <elmarbout.adib@gmail.com>
2023-05-08 19:09:12 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
 
 	buff = make_buffers(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	r = read(from, buff, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
